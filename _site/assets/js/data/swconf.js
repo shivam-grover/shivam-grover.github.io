3b3ddede4c733dd50efc44ab52fda11fdc4c0f23
Shivam Grover <shivumgrover99@gmail.com>
2024-11-07 19:55:09 -0500

    create an empty commit

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1731026636',resources: [
+    cacheName: 'chirpy-1731027308',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
