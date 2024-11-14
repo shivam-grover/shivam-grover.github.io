e0e00a973f25b28a4a42184ee4ed97aa24b66b67
Shivam Grover <shivumgrover99@gmail.com>
2024-11-14 02:35:14 -0500

    Update

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1731032677',resources: [
+    cacheName: 'chirpy-1731569688',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
