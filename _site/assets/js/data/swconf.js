911dc9afe5c1d07324df5f0bf980947a152296f9
Shivam Grover <shivumgrover99@gmail.com>
2024-11-14 02:41:12 -0500

    Remove jekyll analytic

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1731569688',resources: [
+    cacheName: 'chirpy-1731570056',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
