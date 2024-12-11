2f1e7243df1818896044708312447d64ed626cda
Shivam Grover <shivumgrover99@gmail.com>
2024-12-11 18:20:55 -0500

    Remove banner

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1733532409',resources: [
+    cacheName: 'chirpy-1733959200',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
